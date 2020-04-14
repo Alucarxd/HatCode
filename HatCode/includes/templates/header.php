8c56aa2947049f7520971d749ae9dcc71ee7eea9
ColoredHat <58672542+Alucarxd@users.noreply.github.com>
2020-04-14 12:55:32 -0500

    Add files via upload

---
+++ HatCode/includes/templates/header.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="manifest" href="site.webmanifest">
+  <link rel="apple-touch-icon" href="icon.png">
+  <!-- Place favicon.ico in the root directory -->
+
+  <link rel="stylesheet" href="css/normalize.css">
+
+  <link rel="stylesheet" href="css/all.min.css">
+
+  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" />
+
+
+  <script src="https://kit.fontawesome.com/e332e40a11.js" crossorigin="anonymous"></script>
+
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans|Oswald|PT+Sans&display=swap" rel="stylesheet">
+
+  <link rel="stylesheet" href="css/main.css">
+
+  <meta name="theme-color" content="#fafafa">
+</head>
+
+<body>
+  <!--[if IE]>
+    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+  <![endif]-->
+
+  <!-- Add your site or application content here -->
+
+  <header class="site-header">
+    <div class="hero">
+      <div class="contenido-header">
+        <nav class="redes-sociales">
+          <a href="#"><i class="fab fa-facebook-f"></i></a>
+          <a href="#"><i class="fab fa-twitter"></i></a>
+          <a href="#"><i class="fab fa-pinterest-p"></i></a>
+          <a href="#"><i class="fab fa-youtube"></i></a>
+          <a href="#"><i class="fab fa-instagram"></i></a>
+        </nav>
+        <div class="informacion-evento">
+          <p class="fecha"><i class="fas fa-calendar-alt"></i> 20/marzo/2020</p>
+          <p class="ciudad"><i class="fas fa-map-marker-alt"></i> Santo Domingo</p>
+          <h1 class="nombre-sitio">ColoredHat</h1>
+          <p class="slogan">Aqui encontraras informacion sobre<span>Open Source</span></p>
+        </div><!-- INFORMACION EVENTO -->
+      </div>
+    </div><!-- HERO -->
+  </header>
+
+  <div class="barra">
+    <div class="contenedor clearfix">
+      <div class="logo">
+        <a href="index.php">
+        <img src="img/logo.svg" alt="AQUI VA NUESTRO LOGO ">
+        </a>
+      </div>
+      <div class="menu-movil">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <nav class="navegacion-principal clearfix ">
+        <a href="#">********</a>
+        <a href="#">*******</a>
+        <a href="#">Sistemas Operativos</a>
+        <a href="registro.php">Subcribete</a>
+      </nav>
+    </div><!-- CONTENEDOR -->
+  </div><!-- BARRA -->
---
+++ HatCode/index.php
@@ -0,0 +1,67 @@
+<?php include_once 'includes/templates/header.php' ?>
+  <section class="seccion contenedor">
+    <h2>Lo mejor sobre Open Source </h2>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus nesciunt qui, architecto sit, sequi saepe
+      libero fugit nostrum, doloremque mollitia quo similique? Consectetur nesciunt aspernatur itaque alias vitae
+      numquam culpa?</p>
+  </section><!-- SECCION -->
+
+        </div><!-- PROGRAMA EVENTO -->
+      </div><!-- CONTENEDOR -->
+    </div><!-- CONTENIDO PROGRAMA-->
+  </section><!-- PROGRAMA-->
+
+  <section class="invitados contenedor seccion">
+    <h2>Sistemas operativos</h2>
+    <ul class="lista-invitados clearfix">
+      <li>
+        <div class="invitado">
+          <img src="img/invitado1.jpg" alt="imagen invitados">
+          <p>UBuntu</p>
+        </div>
+      </li>
+      <li>
+        <div class="invitado">
+          <img src="img/invitado2.jpg" alt="imagen invitados">
+          <p>Fedora</p>
+        </div>
+      </li>
+      <li>
+        <div class="invitado">
+          <img src="img/invitado3.jpg" alt="imagen invitados">
+          <p>St*********</p>
+        </div>
+      </li>
+      <li>
+        <div class="invitado">
+          <img src="img/invitado4.jpg" alt="imagen invitados">
+          <p>St********</p>
+        </div>
+      </li>
+      <li>
+        <div class="invitado">
+          <img src="img/invitado5.jpg" alt="imagen invitados">
+          <p>ST*******</p>
+        </div>
+      </li>
+      <li>
+        <div class="invitado">
+          <img src="img/invitado6.jpg" alt="imagen invitados">
+          <p>ST****</p>
+        </div>
+      </li>
+    </ul>
+  </section>
+
+  <div id="mapa" class="mapa"></div>
+
+  
+  <div class="newsletter parallax">
+    <div class="contenido contenedor">
+      <p> registrarte a nuestro sitio </p>
+      <h3>Hat Code</h3>
+      <a href="#" class="button transparente">Registro</a>
+    </div><!-- CONTENIDO -->
+  </div><!-- NEWLETTER -->
+
+<?php include_once 'includes/templates/footer.php'; ?>
